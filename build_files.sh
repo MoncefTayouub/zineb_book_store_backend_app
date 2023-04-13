2076edb5bb4c4a1a85d34285304dbdb7afda0c5d
moncefWitcher <moncef.tayoub2@aiesec.net>
2023-04-13 14:50:56 +0000

    pip3 install djangorestframework

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
  echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
  python3.9 -m pip install django
+ python3.9 -m pip install djangorestframework
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"

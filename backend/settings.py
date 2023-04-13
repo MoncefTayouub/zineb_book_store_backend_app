d93f27ed93c1b0d52bcca89ee25e84ba77ecd1a7
moncefWitcher <moncef.tayoub2@aiesec.net>
2023-04-13 14:25:04 +0000

    a try

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9f!u=d4#k5rp30806rz#^o)*tmuams#0)t#mja(sdwa%bu&*jw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.app'
 
 
 # Database
